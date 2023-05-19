7d359f6ada9dcd7a7d6ec57cd0ffd0323ab9a31e
mohammedafsalk <techafs456@gmail.com>
2023-05-19 10:29:07 +0530

    minus button added'

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ function App() {
   function add() {
     setCount(count + 1);
   }
+  function minus() {
+    if (count === 0) return;
+    setCount(count - 1);
+  }
   return (
     <div>
       <button onClick={add}>Add</button>
       <h1>Counter:{count}</h1>
+      <button onClick={minus}>Minus</button>
     </div>
   );
 }
