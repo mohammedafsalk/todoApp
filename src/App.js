0233fd6294f8f90e0fad012deb7a507cc084f161
mohammedafsalk <techafs456@gmail.com>
2023-05-20 13:52:15 +0530

    useeefect commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import './style.css'
+import { useState,useEffect } from "react";
+import "./style.css";
 
 function App() {
-  return(
-    <>
+  const [content,setContent] = useState("")
+  const [values,setValues] = useState([])
 
-    </>
-  )
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${content}`)
+      .then(response => response.json())
+      .then(json => setValues(json))
+  },[content])
+
+
+  return (
+    <div>
+      <div>
+        <button onClick={()=> setContent("Posts")}>Posts</button>
+        <button onClick={()=> setContent("Users")}>Users</button>
+        <button onClick={()=> setContent("Comments")}>Comments</button>
+      </div>
+      <h1>{content}</h1>
+      {values.map(item =>{
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+    </div>
+  );
 }
 
 export default App;
