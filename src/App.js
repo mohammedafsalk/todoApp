7f097caa896f1328683e246cf8136bf913abcd56
mohammedafsalk <techafs456@gmail.com>
2023-05-20 15:07:30 +0530

    structure added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
-function App() {}
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Task</label>
+          <input type="text" name="" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+
+      <h1 className="header">To Do List</h1>
+
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
 
 export default App;
