a015bc598c197b3b3495e0c30458d26fb5cbdf95
mohammedafsalk <techafs456@gmail.com>
2023-05-20 12:56:58 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import React from "react";
+import './style.css'
 
 function App() {
   return(
     <>
-    <Header/>
-    <Main/>
-    <Footer/>
+
     </>
   )
 }
