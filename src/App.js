9a74e800f955421261295a71e13c04c12565447a
mohammedafsalk <techafs456@gmail.com>
2023-05-20 14:51:56 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-import { useState,useEffect } from "react";
-import "./style.css";
-
-function App() {
-  const [content,setContent] = useState("")
-  const [values,setValues] = useState([])
-
-  useEffect(()=>{
-    fetch(`https://jsonplaceholder.typicode.com/${content}`)
-      .then(response => response.json())
-      .then(json => setValues(json))
-  },[content])
-
-
-  return (
-    <div>
-      <div>
-        <button onClick={()=> setContent("Posts")}>Posts</button>
-        <button onClick={()=> setContent("Users")}>Users</button>
-        <button onClick={()=> setContent("Comments")}>Comments</button>
-      </div>
-      <h1>{content}</h1>
-      {values.map(item =>{
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
-    </div>
-  );
-}
+function App() {}
 
 export default App;
