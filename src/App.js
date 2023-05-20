5c41d9eaa1407aac267e9fa00ef070f0188734ab
mohammedafsalk <techafs456@gmail.com>
2023-05-20 11:34:10 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,3 @@
-import { useState } from "react";
-import Counter from "./Counter";
-
-function App() {
-  const [count, setCount] = useState(0);
-  function handleAdd() {
-    setCount(count + 1);
-  }
-  function handleMinus() {
-    if (count === 0) return;
-    setCount(count - 1);
-  }
-  return (
-    <div>
-      <button onClick={handleAdd}>Add</button>
-      <Counter count={count} />
-      <button onClick={handleMinus}>Minus</button>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
