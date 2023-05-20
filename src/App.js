2949fb3967973f5afa409dfaa9c91b16d4cbbb46
mohammedafsalk <techafs456@gmail.com>
2023-05-20 11:52:53 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-function App() {}
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return(
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div>Logo</div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Log out</li>
+      </ul>
+    </>
+  );
+}
+
+export default Header;
