5cac3e5765b18bf0481fe895972d9138352fc22e
mohammedafsalk <techafs456@gmail.com>
2023-05-21 15:00:52 +0530

    css changed

--- src/App.js
+++ src/App.js
@@ -2,35 +2,7 @@ import { useState } from "react";
 import "./style.css";
 
 function App() {
-  const [item, addItem] = useState("");
-  return (
-    <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Task</label>
-          <input
-            value={item}
-            type="text"
-            onChange={(e) => addItem(e.target.value)}
-            id="item"
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-
-      <h1 className="header">To Do List</h1>
-
-      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
